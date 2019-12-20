aef13b5b1a2b0bee9e0e1c62113e7dcc6c15f056
misaelsarm <misaelsarmiento97@gmail.com>
2019-12-19 21:01:37 -0600

    Se agrego funcionalidad al formulario de contacto. Se esta trabajando en un modal para el mensaje de informacion enviada

--- main.js
+++ main.js
@@ -1,38 +1,78 @@
-/* import * as firebase from 'firebase';
+(function() {
+    const firebaseConfig = {
+        apiKey: "AIzaSyCnT6uoSdb-e15lWRI_pSky_SzqNTuHEZo",
+        authDomain: "sellery-92785.firebaseapp.com",
+        databaseURL: "https://sellery-92785.firebaseio.com",
+        projectId: "sellery-92785",
+        storageBucket: "sellery-92785.appspot.com",
+        messagingSenderId: "608603868199",
+        appId: "1:608603868199:web:261ab7403af0e4eac01202",
+        measurementId: "G-XTXW0HJRJ1"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
 
-var firebaseConfig = {
-    apiKey: "AIzaSyCnT6uoSdb-e15lWRI_pSky_SzqNTuHEZo",
-    authDomain: "sellery-92785.firebaseapp.com",
-    databaseURL: "https://sellery-92785.firebaseio.com",
-    projectId: "sellery-92785",
-    storageBucket: "sellery-92785.appspot.com",
-    messagingSenderId: "608603868199",
-    appId: "1:608603868199:web:261ab7403af0e4eac01202",
-    measurementId: "G-XTXW0HJRJ1"
-};
-Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics(); */
-import firebase from 'firebase'
-require('firebase/auth')
-var vm = new Vue({
-    el: "#app",
-    data: {
-        name: "",
-        email: "",
-        cellphone: "",
-        password: "",
-        cpassword: ""
+    let mensajeFormulario = firebase.database().ref('mensajes');
 
-    },
-    methods: {
-        signUp() {
-            firebase.auth().createUserWithEmailAndPassword(this.name, this.email).then(user => {
-                    alert(`You are logged in as ${user.user.email}`);
-                },
-                err => {
-                    alert(err.message);
-                });
+
+
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            registro: {
+                name: "",
+                email: "",
+                cellphone: "",
+                password: "",
+                cpassword: ""
+            },
+            contacto: {
+                name: "",
+                cellphone: "",
+                email: "",
+                website: "",
+                company: "",
+            }
+
+        },
+        methods: {
+            signUp() {
+                const auth = firebase.auth();
+                auth.createUserWithEmailAndPassword(this.registro.email, this.registro.password).then(user => {
+                        alert('Cuenta creada con éxito');
+                    },
+                    err => {
+                        alert(err.message);
+                    });
+            },
+
+            saveMessage(name, cellphone, email, website, company) {
+                let newMessageRef = mensajeFormulario.push();
+                newMessageRef.set({
+                    name: name,
+                    cellphone: cellphone,
+                    email: email,
+                    website: website,
+                    company: company,
+                })
+            },
+            submitForm(e) {
+                e.preventDefault();
+                let name = this.contacto.name;
+                let cellphone = this.contacto.cellphone;
+                let email = this.contacto.email;
+                let website = this.contacto.website;
+                let company = this.contacto.company;
+                this.saveMessage(name, cellphone, email, website, company);
+                //document.getElementById("modal").style.visibility = "visible";
+                this.contacto.name = "";
+                this.contacto.cellphone = "";
+                this.contacto.email = "";
+                this.contacto.website = "";
+                this.contacto.company = "";
+            },
+
         }
-    }
-})
+    })
+    document.getElementById("submitForm").addEventListener('submit', vm.submitForm);
+}())
