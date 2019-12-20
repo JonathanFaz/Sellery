dd77fe133982ff01a7efba6562fb14a8bd2abc76
misaelsarm <misaelsarmiento97@gmail.com>
2019-12-20 16:24:56 -0600

    Se agrego la funcionalidad del formulario de contacto

--- main.js
+++ main.js
@@ -17,62 +17,84 @@
 
 
     var vm = new Vue({
-        el: "#app",
-        data: {
-            registro: {
-                name: "",
-                email: "",
-                cellphone: "",
-                password: "",
-                cpassword: ""
-            },
-            contacto: {
-                name: "",
-                cellphone: "",
-                email: "",
-                website: "",
-                company: "",
-            }
+            el: "#app",
+            data: {
+                registro: {
+                    name: "",
+                    email: "",
+                    cellphone: "",
+                    password: "",
+                    cpassword: ""
+                },
+                contacto: {
+                    name: "",
+                    cellphone: "",
+                    email: "",
+                    website: "",
+                    company: "",
+                }
 
-        },
-        methods: {
-            signUp() {
-                const auth = firebase.auth();
-                auth.createUserWithEmailAndPassword(this.registro.email, this.registro.password).then(user => {
-                        alert('Cuenta creada con éxito');
-                    },
-                    err => {
-                        alert(err.message);
+            },
+            methods: {
+                signUp() {
+                    const auth = firebase.auth();
+                    auth.createUserWithEmailAndPassword(this.registro.email, this.registro.password).then(user => {
+                            alert('Cuenta creada con éxito');
+                        },
+                        err => {
+                            alert(err.message);
+                        });
+                },
+                login(e) {
+                    e.preventDefault();
+                    firebase
+                        .auth()
+                        .signInWithEmailAndPassword(this.registro.email, this.registro.password)
+                        .then(
+                            user => {
+                                alert("Welcome");
+                                //this.$router.push("/");
+                            },
+                            err => {
+                                alert(err.message);
+                            }
+                        );
+
+                },
+
+                saveMessage(name, cellphone, email, website, company) {
+                    let newMessageRef = mensajeFormulario.push();
+                    newMessageRef.set({
+                        name: name,
+                        cellphone: cellphone,
+                        email: email,
+                        website: website,
+                        company: company,
+                    })
+                },
+                submitForm(e) {
+                    e.preventDefault();
+                    let name = this.contacto.name;
+                    let cellphone = this.contacto.cellphone;
+                    let email = this.contacto.email;
+                    let website = this.contacto.website;
+                    let company = this.contacto.company;
+                    this.saveMessage(name, cellphone, email, website, company);
+                    document.getElementById("modal").style.visibility = "visible";
+                    document.getElementById("overlay").style.visibility = "visible";
+                    let aceptar = document.getElementById("aceptar").addEventListener('click', function() {
+                        document.getElementById("modal").style.visibility = "hidden";
+                        document.getElementById("overlay").style.visibility = "hidden";
                     });
-            },
+                    this.contacto.name = "";
+                    this.contacto.cellphone = "";
+                    this.contacto.email = "";
+                    this.contacto.website = "";
+                    this.contacto.company = "";
+                },
 
-            saveMessage(name, cellphone, email, website, company) {
-                let newMessageRef = mensajeFormulario.push();
-                newMessageRef.set({
-                    name: name,
-                    cellphone: cellphone,
-                    email: email,
-                    website: website,
-                    company: company,
-                })
-            },
-            submitForm(e) {
-                e.preventDefault();
-                let name = this.contacto.name;
-                let cellphone = this.contacto.cellphone;
-                let email = this.contacto.email;
-                let website = this.contacto.website;
-                let company = this.contacto.company;
-                this.saveMessage(name, cellphone, email, website, company);
-                //document.getElementById("modal").style.visibility = "visible";
-                this.contacto.name = "";
-                this.contacto.cellphone = "";
-                this.contacto.email = "";
-                this.contacto.website = "";
-                this.contacto.company = "";
-            },
-
-        }
-    })
-    document.getElementById("submitForm").addEventListener('submit', vm.submitForm);
+            }
+        })
+        //document.getElementById("submitForm").addEventListener('submit', vm.submitForm);
+        //document.getElementById("login").addEventListener('submit', vm.login);
 }())
